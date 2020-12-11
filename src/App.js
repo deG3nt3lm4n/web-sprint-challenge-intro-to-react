551c48a218610528e161241dd759cafb5bf6fb31
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2020-12-11 18:11:17 -0500

    kinda of struggling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+
+const pokemonUrl = 'https://pokeapi.co/api/v2/pokemon/'
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [pokemonData, setPokemonData] = useState([])
+
+  // Page of pokemon max 20?
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=200')
+      .then(res => setPokemonData(res.data.results))
+      .catch(err => console.log(err))
+
+      return(
+        console.log('clean up')
+      )
+  },[])
+
+  console.log(pokemonData)
+
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      <p>Pokemon api v1</p>
+
+    <header>
+      <h1>Pokemon App</h1>
+      <p>Find your stupid pokemon</p>
+      <select>
+        <option>Pokemon 1</option>
+        <option>Pokemon 2</option>
+        <option>Pokemon 3</option>
+      </select>
+    </header>
+
+    <div className="PokemonCard">
+      <div className="PokemonCardInner">
+
+        <div className="PokemonCardImage">
+          <img src="" alt="pokemon img" />
+        </div>
+
+        <div className="PokemonCardContent">
+            <h2>Name</h2>
+        </div>
+
+      </div>
+    </div>
+
     </div>
   );
 }
